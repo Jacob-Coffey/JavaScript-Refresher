75b283c4596126c8f3c6d3a957f48b5a4cc14158
Jacob Coffey <Jacob23.coffey@gmail.com>
2022-07-06 22:31:23 -0400

    first commit finished homework

---
+++ script.js
@@ -0,0 +1,22 @@
+const firstName = "Jacob";
+const lastName = "Coffey";
+const fullName = (firstName + " " + lastName);
+
+console.log(fullName);
+
+const population = "2000000";
+
+console.log(population);
+console.log(population * 3);
+
+const javaScriptIsCool = true;
+
+const colors = [ "Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet" ];
+
+for (let i = 1; i <= 5; i++) {
+console.log(i);
+}
+
+for (let x = 0; x <= 4; x++) {
+    console.log(x);
+    }
